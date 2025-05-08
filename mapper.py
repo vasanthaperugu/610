3682f62a15c359a79958b86f574d9ef7dec3d6b6
vasanthaperugu <62531022+vasanthaperugu@users.noreply.github.com>
2025-05-07 21:23:22 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-
-    # split the line into words; splits on any whitespace
     words = line.split()
-
-    # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        print(f'{word}\t1')
